fb42f8fb30de5c4903ec8ecf8945368006e8bd28
apsamuel <aaron.psamuel@gmail.com>
2022-10-23 15:54:23 -0400

    chore: refactor tools

---
+++ tools/lib/createTable.js
@@ -0,0 +1,34 @@
+import AWS from 'aws-sdk';
+import { Params } from '../data/Params.js'
+
+export const createTable = async ({
+  name = 'darkphoton',
+  hash = '_pk',
+  range = '_sk',
+  debug = false
+}) => {
+
+  if (debug) console.log(`creating table ${name} with hash key ${hash} and range key ${range}...`);
+  const databaseClient = new AWS.DynamoDB({
+    endpoint: new AWS.Endpoint(process.env.DYNAMODB_ENDPOINT)
+  });
+
+  const params = Params({
+    name,
+    hash,
+    range
+  });
+
+  const run = async () => {
+    try {
+      console.log(`Creating table ${params.TableName}...`);
+      const data = await databaseClient.createTable(params).promise();
+      console.log(data);
+      return data
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  await run();
+}
