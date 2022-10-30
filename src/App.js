1bf325c6af08ddd2d5429f5b5857e74fdf250921
apsamuel <aaron.psamuel@gmail.com>
2022-10-30 16:14:05 -0400

    chore: init App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import logo from './logo.svg'
+import './App.css'
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import {
+  Configuration
+} from './config/Configuration.js'
+
+import {
+  createTheme,
+  ThemeProvider,
+  Box,
+  CssBaseline
+} from '@mui/material'
+
+import {
+  ControlBar
+} from './components/ControlBar.jsx'
+
+App.propTypes = {
+  colors: PropTypes.string,
+  config: PropTypes.object,
+  title: PropTypes.string,
+  subtitle: PropTypes.string
+}
+export function App ({
+  colors = 'dark',
+  // config = {},
+  ...props
+}) {
+  const theme = createTheme({
+    ...Configuration.themes.find(theme => theme.name === 'default').theme
+  })
+  // const config = config || Configuration
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <ControlBar
+        theme={theme}
+        config={Configuration}
+      />
+      <Box className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {Configuration.title}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            More
+          </a>
+        </header>
+      </Box>
+    </ThemeProvider>
+
+  )
 }
 
-export default App;
+export default App
